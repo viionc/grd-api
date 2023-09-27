43f4de830dbe797bfd0f9596a3af759ba954112b
viionc <marek.d.stankiewicz@gmail.com>
2023-09-27 19:38:41 +0200

    changed file hierarchy

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const {getRandomFirstName} = require("./util/firstNames");
-const {getRandomLastName} = require("./util/lastNames");
-const {getRandomCountry} = require("./util/countries");
-const {logger} = require("./util/logger");
+const {getRandomFirstName} = require("../util/firstNames");
+const {getRandomLastName} = require("../util/lastNames");
+const {getRandomCountry} = require("../util/countries");
+const {logger} = require("../util/logger");
 
 app.use(express.static("./public"));
 app.use(logger);
